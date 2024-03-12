6747c1fc35f230378aaa7c3fd09e62d3c497d90b
soni kumari <soniindu92@gmail.com>
2024-03-12 21:53:48 +0530

    129: Entity class created

---
+++ src/main/java/com/myblog22/entity/Post.java
@@ -0,0 +1,20 @@
+package com.myblog22.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
