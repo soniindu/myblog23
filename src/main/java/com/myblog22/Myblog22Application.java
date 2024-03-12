8a97d1c3290a915ed52f0eaffffe6886b7c9ccfe
soni kumari <soniindu92@gmail.com>
2024-03-12 21:30:25 +0530

    129: database created

---
+++ src/main/java/com/myblog22/Myblog22Application.java
@@ -0,0 +1,13 @@
+package com.myblog22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog22Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog22/Myblog22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myblog22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
